be5fbe6f4c7e277d76bea3ed9bc60ff55048bfa2
Alex Angas <alexangas@gmail.com>
2018-11-05 17:22:21 +0000

    Initial copy

---
+++ src/PodFeedReader/Helpers/Xml/XmlHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PodApp.Data.Collection.Helpers.Xml
+{
+    public static class XmlHelper
+    {
+        public static XDocument Parse(string raw)
+        {
+            XDocument xDocument;
+
+            var xmlReaderSettings = new XmlReaderSettings
+            {
+                CheckCharacters = false,
+                DtdProcessing = DtdProcessing.Ignore,
+                IgnoreComments = true,
+                IgnoreProcessingInstructions = true,
+                IgnoreWhitespace = true,
+                ValidationType = ValidationType.None,
+            };
+
+            using (var stringReader = new StringReader(raw))
+            using (XmlReader xmlReader = XmlReader.Create(stringReader, xmlReaderSettings))
+            {
+                xmlReader.MoveToContent();
+                try
+                {
+                    xDocument = XDocument.Load(xmlReader, LoadOptions.SetLineInfo);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return xDocument;
+        }
+
+        // From https://stackoverflow.com/a/9335141/6651
+        public static IEnumerable<XElement> ElementsCaseInsensitive(this XContainer source, XName name)
+        {
+            foreach (XElement e in source.Elements())
+            {
+                if (e.Name.Namespace.NamespaceName.Equals(name.Namespace.NamespaceName, StringComparison.OrdinalIgnoreCase) &&
+                    e.Name.LocalName.Equals(name.LocalName, StringComparison.OrdinalIgnoreCase))
+                    yield return e;
+            }
+        }
+
+        public static IEnumerable<XElement> DescendantsCaseInsensitive(this XContainer source, XName name)
+        {
+            foreach (XElement e in source.Descendants())
+            {
+                if (e.Name.Namespace.NamespaceName.Equals(name.Namespace.NamespaceName, StringComparison.OrdinalIgnoreCase) &&
+                    e.Name.LocalName.Equals(name.LocalName, StringComparison.OrdinalIgnoreCase))
+                    yield return e;
+            }
+        }
+    }
+}
---
+++ src/PodFeedReader/Readers/InvalidPodcastFeedException.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace PodApp.Data.Collection.Readers
+{
+    public class InvalidPodcastFeedException : Exception
+    {
+        public string Contents { get; set; }
+
+        public enum InvalidPodcastFeedReason : short
+        {
+            Unknown = 0,
+            UnexpectedEndOfStream = 1,
+            UnexpectedByteCount = 2,
+            HtmlDocument = 3,
+            FeedStartNotFound = 4,
+            ShowStartNotFound = 5,
+            EpisodeStartNotFound = 5,
+            ShowContentTooLong = 7,
+            EpisodeContentTooLong = 7,
+        }
+
+        public InvalidPodcastFeedException()
+        {
+        }
+
+        public InvalidPodcastFeedException(string message) : base(message)
+        {
+        }
+
+        public InvalidPodcastFeedException(InvalidPodcastFeedReason reason) : base($"{reason}")
+        {
+        }
+
+        public InvalidPodcastFeedException(InvalidPodcastFeedReason reason, string contents) : this(reason)
+        {
+            Contents = contents;
+        }
+    }
+}
